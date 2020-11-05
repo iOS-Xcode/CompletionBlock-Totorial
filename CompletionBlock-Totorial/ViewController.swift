11d4ca0beeea70d015b2afe2186c6078b7003ba6
Seokhyun Kim <wjapp72@gmail.com>
2020-11-04 21:10:07 -0800

    #CompletionBlock and asynchronous task with activity indicator.

--- CompletionBlock-Totorial/ViewController.swift
+++ CompletionBlock-Totorial/ViewController.swift
@@ -13,20 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var hello: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Called 1
         print("viewController - viewDidLoad callled")
         
         KRProgressHUD.show()
         
         sayHi(completion: { result in
+            //Called 4
             print("It is from completion block \(result)")
             KRProgressHUD.showSuccess()
             self.hello.text = result
         })
+        //Called 3
         print("Is this the end? or not")
     }
     //Asynchronous process and completion block with send with string
 
     fileprivate func sayHi(completion : @escaping(String) -> () ) {
+        //Called 2
         print("viewController - sayHi Method callled")
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // Change `2.0` to the desired number of seconds.
